404aced74d7f62d71611cf0ac1f7d7dc21ec9e9c
cocksure <sanjarwer93@gmail.com>
2023-07-15 09:48:28 +0500

    some changesto  the django-environ

--- berries_project/settings.py
+++ berries_project/settings.py
@@ -1,11 +1,15 @@
 from pathlib import Path
-from environ import Env
+import environ
+import os
 
-env = Env()
-env.read_env('.env')
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -130,24 +134,5 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000/", ]
 
 
-def verified_callback(user):  # Emailni tasdiqlash funkisiyasi
-    user.is_active = True
-
-
-EMAIL_VERIFIED_CALLBACK = verified_callback  # qachonki funksiya to'g'ri bajarilganda yuqoridagi funksiyani ishga tushuradi.
-EMAIL_FROM_ADDRESS = 'AdaptaMail@gmail.com'
-EMAIL_MAIL_SUBJECT = 'Confirm your email, {{user.username}}'  # Email xabarnoma nomi
-EMAIL_MAIL_HTML = 'mail_body.html'  # Email tasdiqlash sahifasi
-EMAIL_MAIL_PLAIN = 'mail_body.txt'  # Emailga jo'natiladigan asosiy matn
-EMAIL_TOKEN_LIFE = 60 * 60  # Tokenni eskitish vaqti
-EMAIL_PAGE_TEMPLATE = 'confirm_template.html'  # Tasdiqlangan keyingi satusni ko'rsatib turadigan sahifa
-EMAIL_PAGE_DOMAIN = 'http://127.0.0.1:8000'  # Domain
-EMAIL_MULTI_USER = True  # optional (defaults to False)
-
 # For Django Email Backend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Konsolga chiqarib turadi.
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'AdaptaMail@gmail.com'
-EMAIL_HOST_PASSWORD = 'uheqxjowmcqdoich'
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
